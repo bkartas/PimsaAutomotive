7dad4692085e7819b462ee9c28cd3b3012b03fbc
bkartas <81967494+bkartas@users.noreply.github.com>
2021-12-07 01:24:25 +0300

    Add files via upload

---
+++ js/Site.js
@@ -0,0 +1,35 @@
+﻿
+
+              jQuery(function ($) {
+                  //ANA SAYFA DEVAMINI OKU
+                  $('.devamyazi').on('click', function () {
+
+                      if ($(this).hasClass('aktif')) {
+                          $('.fullscreen-topnav .text-item p').css({ 'height': '100px' });
+                          $(this).removeClass('aktif');
+                      } else {
+                          $('.fullscreen-topnav .text-item p').css({ 'height': 'auto' });
+                          $(this).addClass('aktif');
+                      }
+
+                  });
+                  //ANA SAYFA DEVAMINI OKU
+              });
+
+
+
+jQuery(function ($) {
+    //ANA SAYFA DEVAMINI OKU
+    $('.devamyazi2').on('click', function () {
+
+        if ($(this).hasClass('aktif')) {
+            $('.item-about .content').css({ 'height': '100px' });
+            $(this).removeClass('aktif');
+        } else {
+            $('.item-about .content').css({ 'height': 'auto' });
+            $(this).addClass('aktif');
+        }
+
+    });
+    //ANA SAYFA DEVAMINI OKU
+});
